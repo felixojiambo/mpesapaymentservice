3f91a0f8244eb9ed7cc03cb3f917eb26a761a337
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-08 09:27:09 -0700

    setup payment module

---
+++ src/main/java/com/mpesa_daraja_api/Payment/Service/PaymentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.mpesa_daraja_api.Payment.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PaymentServiceApplication.class, args);
+		System.out.println("daraja api")
+	}
+
+}
---
+++ src/test/java/com/mpesa_daraja_api/Payment/Service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpesa_daraja_api.Payment.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
